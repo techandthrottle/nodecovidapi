9915a826912448cf587d619aa8620e81ea99918b
Rider's Who Code <rootxenon@gmail.com>
2020-06-11 18:44:02 +0800

    Upload the original server.js

--- server.js
+++ server.js
@@ -1,36 +1,30 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require('express')
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 5000
 
 
-require('dotenv').config();
+app.use(express.json())
 
-const app = express();
-const port = process.env.PORT || 5000;
+const uri = process.env.MONGO_URI
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true })
 
-app.use(cors());
-app.use(express.json());
-
-const uri = process.env.MONGO_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
-
-const connection = mongoose.connection;
-
+const connection = mongoose.connection
 
 connection.once('open', () => {
-    console.log("MongoDB connection established successfully");
-});
+    console.log('MongoDB connection established successfully')
+})
 
-const profileRouter = require('./routes/profile.route');
-const userRouter = require('./routes/user.route');
-const covidRouter = require('./routes/data_maintenance.route');
-const covidOperationRouter = require('./routes/operation.route');
 
-app.use('/profile', profileRouter);
-app.use('/user', userRouter);
-app.use('/covid', covidRouter);
-app.use('/covid', covidOperationRouter);
+const covidRouter = require('./routes/data_maintenace.route')
+const operationRouter = require('./routes/operation.route')
+app.use('/covid', covidRouter)
+app.use('/covid', operationRouter)
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+    console.log(`Node Server is running on localhost: ${port}`)
 })
+
