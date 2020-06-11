5fee8cc4a1f88f02dda5ed3bbf78e43c361b41e0
Rider's Who Code <rootxenon@gmail.com>
2020-06-11 18:46:26 +0800

    Update data_maintenance.route.js

--- routes/data_maintenance.route.js
+++ routes/data_maintenance.route.js
@@ -1,9 +1,8 @@
-const router = require('express').Router();
+const router = require('express').Router()
 
-const covidController = require('../controller/data_maintenance.controller');
+const data_maintenanceController = require('../controller/data_maintenance.controller.js')
 
+router.route('/maintenance/summary').get(data_maintenanceController.covid_summary)
 
-router.route('/maintenance/summary').get(covidController.covid_summary);
-router.route('/maintenance/datadrop').get(covidController.covid_doh_data_drop);
+module.exports = router
 
-module.exports = router;
